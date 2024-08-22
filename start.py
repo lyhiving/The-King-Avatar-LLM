da6c730f8f4b29a8489b4befa67769424e6651d2
lyhiving <lyhiving@users.noreply.github.com>
2024-08-22 14:26:06 +0800

    优化端口映射

--- start.py
+++ start.py
@@ -1,4 +1,4 @@
 import os
 
 # os.system('streamlit run app.py --server.address=127.0.0.1 --server.port 9999')
-os.system('streamlit run app.py ')
+os.system('streamlit run app.py  --server.enableCORS false --server.port 6006')
